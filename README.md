98b83eb5fc0bbbcaf2f5eea7c14b763b271398e9
Zander Franks <me@zanderf.net>
2023-12-17 17:49:25 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# priority-slots
+
+Priority player slots for players with configurable roles. Save any number of
+slots in your server for the host or players with predetermined roles.
+
+## Install
+
+`omegga install gh:voximity/priority-slots`
+
+## Usage
+
+Configure the plugin from the Omegga web UI.
+
+## Credits
+
+Made for base4's New Years 2024 server.
---
+++ omegga.plugin.ts
@@ -0,0 +1,54 @@
+import OmeggaPlugin, { OL, PS, PC, OmeggaPlayer } from 'omegga';
+
+type Config = { slots: number; message: string; roles: string[] };
+
+export default class Plugin implements OmeggaPlugin<Config, {}> {
+  omegga: OL;
+  config: PC<Config>;
+  store: PS<{}>;
+
+  constructor(omegga: OL, config: PC<Config>, store: PS<{}>) {
+    this.omegga = omegga;
+    this.config = config;
+    this.store = store;
+  }
+
+  isPriority = (user: OmeggaPlayer | string) => {
+    const p = typeof user === 'string' ? this.omegga.getPlayer(user) : user;
+    return (
+      p.isHost() || p.getRoles().some((r) => this.config.roles.includes(r))
+    );
+  };
+
+  getPlayerCount = async (): Promise<[number, number]> => {
+    const match = await Omegga.watchLogChunk(
+      'Server.Status',
+      /Players \((\d+)\/(\d+)\):$/,
+      { first: (match) => match[0].startsWith('Players ('), timeoutDelay: 1000 }
+    );
+
+    return [Number(match[0][1]), Number(match[0][2])];
+  };
+
+  async init() {
+    this.omegga.on('join', async ({ id }) => {
+      setTimeout(async () => {
+        const player = this.omegga.getPlayer(id);
+        if (this.isPriority(player)) return;
+
+        const [count, max] = await this.getPlayerCount();
+        if (count > max - this.config.slots) {
+          this.omegga.writeln(
+            `Chat.Command /Kick "${player.name}" "${this.config.message
+              .replace(/\{\}/g, this.config.slots.toString())
+              .replace('"', '\\"')}"`
+          );
+        }
+      });
+    });
+
+    return {};
+  }
+
+  async stop() {}
+}
