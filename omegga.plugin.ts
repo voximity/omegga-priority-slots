8b99d1cb07be5e5282b751747411c02f30114aa0
Zander Franks <me@zanderf.net>
2023-12-31 13:48:14 -0600

    feat: setting to stop kicking players after slots are filled

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,6 +1,11 @@
 import OmeggaPlugin, { OL, PS, PC, OmeggaPlayer } from 'omegga';
 
-type Config = { slots: number; message: string; roles: string[] };
+type Config = {
+  slots: number;
+  message: string;
+  roles: string[];
+  only_if_slots_unfilled: boolean;
+};
 
 export default class Plugin implements OmeggaPlugin<Config, {}> {
   omegga: OL;
@@ -8,6 +13,7 @@ export default class Plugin implements OmeggaPlugin<Config, {}> {
   store: PS<{}>;
 
   cachedCount: number;
+  priorityOnline: Record<string, boolean>;
 
   constructor(omegga: OL, config: PC<Config>, store: PS<{}>) {
     this.omegga = omegga;
@@ -53,13 +59,36 @@ export default class Plugin implements OmeggaPlugin<Config, {}> {
   };
 
   async init() {
+    // get the number of priority online
+    this.priorityOnline = {};
+    for (const player of this.omegga.getPlayers()) {
+      if (this.isPriority(player.id)) {
+        this.priorityOnline[player.id] = true;
+      }
+    }
+    console.log(
+      'There are',
+      Object.keys(this.priorityOnline).length,
+      'priority players online'
+    );
+
     this.omegga.on('join', async ({ id }) => {
       setTimeout(async () => {
         try {
           const player = this.omegga.getPlayer(id);
-          if (this.isPriority(player)) return;
+          if (this.isPriority(player)) {
+            this.priorityOnline[id] = true;
+          }
 
           const [count, max] = await this.getPlayerCount();
+
+          // don't kick players if the slots are filled and we have that setting on
+          if (
+            this.config.only_if_slots_unfilled &&
+            Object.keys(this.priorityOnline).length >= this.config.slots
+          )
+            return;
+
           if (count > max - this.config.slots) {
             this.omegga.writeln(
               `Chat.Command /Kick "${player.name}" "${this.config.message
@@ -70,7 +99,11 @@ export default class Plugin implements OmeggaPlugin<Config, {}> {
         } catch (e) {
           this.errorMessage(e);
         }
-      });
+      }, 500);
+    });
+
+    this.omegga.on('leave', async ({ id }) => {
+      delete this.priorityOnline[id];
     });
 
     return {};
